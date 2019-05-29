f3bce239befa78dd8c8a52d8fe455cfafa1c72dc
OscarXVI <calavera-123>
2019-05-29 17:34:47 -0500

    Se realiza el primer commit con el splash de inicio

--- app/src/main/java/com/example/recetumapp/MainActivity.java
+++ app/src/main/java/com/example/recetumapp/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.recetumapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_progress_bar);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        progressBar = findViewById(R.id.progress_bar);
+        textView = findViewById(R.id.text_view);
+
+        progressBar.setMax(100);
+        progressBar.setScaleY(3f);
+
+        progressAnimation();
     }
+    public void progressAnimation(){
+
+        ProgressBarAnimation anim= new ProgressBarAnimation(this, progressBar, textView,   0f, 100f);
+        anim.setDuration(1000);
+        progressBar.setAnimation(anim);
+    }
+
 }
